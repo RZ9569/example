0a3f4fc875421a1447b3682f1c0b0104565afb0d
Rustem Zhexembin <rustemzhexembin@MacBook-Air-Rustem.local>
2022-11-19 17:54:51 +0600

    example with true false conditions added

---
+++ Example005_ConditionsFLs/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "leila")
+{
+    Console.WriteLine("Exciting, it is Leila!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
